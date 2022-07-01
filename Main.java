420989cb9a58a9b902bbb3acc6777c86ffe86b98
Hettore Eduardo <duh.barretoo@gmail.com>
2022-07-01 16:55:15 -0300

    Adicionei a funcionalidade de multiplicação!

--- Main.java
+++ Main.java
@@ -12,6 +12,7 @@ public class Main {
 		
 		//operador = sc.next();
 		//use a primeira posição (C) para adicionar a operação que são respectivamente 1 para somar e 2 para subtrair.
+		//System.out.println("Por favor para usar a calculadora vc deve apertar enter após essa mensagem e escolher a operação desejada usando o numero 1 para somar o 2 para subtrair ou o 3 para multiplicar");
 		
 		C = sc.nextInt();
 		A = sc.nextInt();
@@ -29,8 +30,11 @@ public class Main {
 			System.out.println("O resultado da subtração é = " + (A - B));
 		}
 		
+		else if (C == 3) {
+			System.out.println("O resultado da multiplicação é = " + (A * B));
+		}
 		else {
-			System.out.println("Por favor informe uma operação valida sendo 1 para somar e 2 para subtrair.");
+			System.out.println("Por favor informe uma operação valida sendo 1 para somar, 2 para subtrair ou 3 para multiplicar.");
 		}
 		
 		
